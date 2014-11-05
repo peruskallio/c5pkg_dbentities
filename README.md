6a8482d4eba18e280982abad924b1b1131fecf5b
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2014-11-05 14:45:25 +0200

    Updated readme.

---
+++ README.md
@@ -0,0 +1,57 @@
+# Doctrine Entities for concrete5 Packages
+
+This package provides simple classes to enable the use of Doctrine entities
+within concrete5 package context. This only works within the concrete5 context
+which means that this cannot be used as a standalone package.
+
+This is only provided as an example implementation of how this could be done
+within the concrete5 package context. We have no plans to publis this into
+Packagist since we hope the functionality that this provides will become part
+of the concrete5 core at some point. But for now (concrete5.7.2), we need some
+external solution to make this possible.
+
+The Entity class provided within this package is a simple class that allows
+accessing the entity variables from outside of the class. All package's entity
+classes should extend this class. It also allows protecting certain class
+variables to be read or written (or both) from outside of that Entity class.
+Please see the class comments for further information.
+
+The Package class is a base package class that extends the concrete5's internal
+Package class. This class provides additional functionality related to the
+package's entity management. This functionality includes providing a package
+specific EntityManager and automatic installation for database tables of the
+entities and automatic migrations of the database during package upgrades. The
+class also provides functionality to automatically generate the package proxy
+classes during the development phase of the package.
+
+
+## How to use
+
+1. Add this library through this git repo to the composer.json of your package
+   and run "composer install"
+2. Make your Package class extend \Mainio\C5\Entity\Package
+3. Create your entities within the /src/Entities directory
+4. Add the following configuration to your /config/app.php:
+```php
+return array(
+    // ...
+    'package_dev_mode' => true,
+    // ...
+);
+```
+
+The last configuration enables the automatic entity proxy class generation for
+your development machine. This does not need to happen in the final version of
+the package as it might cause some problems regarding the directory level
+permissions required for it to happen.
+
+An example on how to use this package is available as a reference
+implementation in the following repository:
+https://github.com/mainio/c5_entities_example
+
+
+## License
+
+Licensed under the MIT license. See LICENSE for more information.
+
+Copyright (c) 2014 Mainio Tech Ltd.
---
+++ src/Mainio/C5/Entity/Package.php
@@ -0,0 +1,315 @@
+<?php
+
+/**
+ * This class provides the ability for packages to automatically install their
+ * DB schema from the entity classes written in PHP. This also creates the
+ * proxy classes for the entities and handles database migrations.
+ * 
+ * There is currently no information for concrete5 developers on the best
+ * practices for package development so it is likely that this will become
+ * obsolete once there is proper information available.
+ * 
+ * However, for now, we'll use this method for ease of use and 
+ * 
+ * This class cannot be used in any other context than the concrete5 context.
+ * This is because it extends the concrete5's internal Package class which is
+ * usually not available outside of the concrete5 context. Also, this class
+ * uses the DIR_PACKAGES constant that is internally set in concrete5.
+ * 
+ * @author Antti Hukkanen <antti.hukkanen(at)mainiotech.fi>
+ * @copyright 2014 Mainio Tech Ltd.
+ * @license MIT
+ */
+
+namespace Mainio\C5\Entity;
+
+use Config;
+use Database;
+
+class Package extends \Concrete\Core\Package\Package
+{
+
+    // The directory within the package where proxies are saved
+    protected $DIRNAME_PROXIES = 'proxies';
+    
+    // Package-specific entity manager
+    protected $entityManager;
+
+    public function install($data = null)
+    {
+        // Only generate the proxy classes when we're developing the package.
+        if (Config::get('app.package_dev_mode')) {
+            $this->generateProxyClasses();
+        }
+        $pkg = parent::install();
+        $this->installDatabase();
+        return $pkg;
+    }
+
+    public function upgrade()
+    {
+        // Only generate the proxy classes when we're developing the package.
+        if (Config::get('app.package_dev_mode')) {
+            $this->generateProxyClasses();
+        }
+        parent::upgrade();
+        $this->installDatabase();
+    }
+
+    public function generateProxyClasses()
+    {
+        $classes = $this->getPackageEntities();
+        
+        if (sizeof($classes) > 0) {
+            // First create the proxies directory if it does not already exist.
+            $proxiesDir = $this->getPackageProxiesPath();
+            if (!is_dir($proxiesDir)) {
+                if (file_exists($proxiesDir)) {
+                    throw new Exception(sprintf(
+                        "A file exists in place of the pacakge proxies directory. " .
+                        "Please remove the file named '%s' from the package.",
+                        $this->DIRNAME_PROXIES
+                    ));
+                }
+                @mkdir($proxiesDir);
+                if (is_dir($proxiesDir)) {
+                    @chmod($proxiesDir, DIRECTORY_PERMISSIONS_MODE);
+                } else {
+                    throw new Exception(
+                        "Could not create the proxies directory. " .
+                        "Please check the file permissions of the package folder."
+                    );
+                }
+            }
+
+            $em = $this->getEntityManager();
+            $cmf = $em->getMetadataFactory();
+
+            $metadatas = array();
+            foreach ($classes as $cls) {
+                $metadatas[] = $cmf->getMetadataFor($cls);
+            }
+
+            // Generate the proxy classes
+            $pf = $em->getProxyFactory();
+            $pf->generateProxyClasses($metadatas);
+        }
+    }
+
+    public function getEntityManager()
+    {
+        if (!isset($this->entityManager)) {
+            $conn = Database::getActiveConnection();
+            $em = $conn->getEntityManager();
+            $config = $em->getConfiguration();
+            $this->modifyEntityManagerConfiguration($config);
+
+            $this->entityManager = \Doctrine\ORM\EntityManager::create($conn, $config);
+        }
+        return $this->entityManager;
+    }
+
+    /** 
+     * Installs the database tables according to the entity schema definitions.
+     * This will not install any existing tables but it will migrate those
+     * tables to matche the current schema definitions for the classes.
+     */
+    public function installDatabase()
+    {
+        $this->dropObsoleteDatabaseTables();
+
+        $classes = $this->getPackageEntities();
+
+        if (sizeof($classes) > 0) {
+            // We need to create the SchemaDiff manually here because we want
+            // to avoid calling the execution for two separate SchemaDiff
+            // objects (one for missing tables and one for new ones).
+            // Also, while $tool->createSchema($missingEntities) works great
+            // for new tables, $tool->updateSchema($updateEntities) would
+            // actually delete all the DB tables that the DB contains and are
+            // not part of the entity tables passed to the function. Therefore,
+            // we do this manually here.
+            $em = $this->getEntityManager();
+            $conn = $em->getConnection();
+            $sm = $conn->getSchemaManager();
+            $cmf = $em->getMetadataFactory();
+
+            $tool = new \Doctrine\ORM\Tools\SchemaTool($em);
+            $comparator = new \Doctrine\DBAL\Schema\Comparator();
+
+            $metadatas = array();
+            foreach ($classes as $cls) {
+                $metadatas[] = $cmf->getMetadataFor($cls);
+            }
+
+            // NOTE: $newSchema != $toSchema because $toSchema would actually
+            // contain each and every table in the database. We'll only need
+            // to traverse the $newSchema for the purposes of the desired
+            // functionality but we also need $fromSchema to check whether
+            // the table already exists and also to get the current schema
+            // for that table to figure out the changes to the new table.
+            $fromSchema = $sm->createSchema();
+            $newSchema = $tool->getSchemaFromMetadata($metadatas);
+
+            $newTables = array();
+            $changedTables = array();
+            foreach ($newSchema->getTables() as $newTable) {
+                // Check if the table already exists
+                if ($fromSchema->hasTable($newTable->getName())) {
+                    $diff = $comparator->diffTable($fromSchema->getTable($newTable->getName()), $newTable);
+                    if ($diff) {
+                        $changedTables[] = $diff;
+                    }
+                } else {
+                    $newTables[] = $newTable;
+                }
+            }
+
+            if (sizeof($newTables) > 0 || sizeof($changedTables) > 0) {
+                // If we have new or changed tables (or both), we'll gather
+                // these DB changes into a SchemaDiff object and get all the
+                // necessary DB migration queries for that diff object.
+                // Finally, those queries are executed against the DB.
+                $schemaDiff = new \Doctrine\DBAL\Schema\SchemaDiff($newTables, $changedTables);
+                $platform = $conn->getDatabasePlatform();
+                $migrateSql = $schemaDiff->toSql($platform);
+                foreach ($migrateSql as $sql) {
+                    $conn->executeQuery($sql);
+                }
+            }
+        }
+    }
+
+    /**
+     * Do not normally call this during a package uninstall. Save this for 
+     * special occasions.
+     * 
+     * This drops all the tables related to this package based on the database
+     * tables of the package entities.
+     */
+    /**
+     * Do not normally call this during a package uninstall. Save this for 
+     * special occasions.
+     * 
+     * This drops all the tables related to this package based on the database
+     * tables of the package entities.
+     */
+    public function uninstallDatabase()
+    {
+        $classes = $this->getPackageEntities();
+
+        if (sizeof($classes) > 0) {
+            $em = $this->getEntityManager();
+            $conn = $em->getConnection();
+            $sm = $conn->getSchemaManager();
+            $cmf = $em->getMetadataFactory();
+
+            $tool = new \Doctrine\ORM\Tools\SchemaTool($em);
+
+            $metadatas = array();
+            foreach ($classes as $cls) {
+                $metadatas[] = $cmf->getMetadataFor($cls);
+            }
+            $newSchema = $tool->getSchemaFromMetadata($metadatas);
+
+            // We'll let Doctrine resolve the correct drop order for the tables
+            // because of which we use the schema migration method to drop the
+            // tables. By letting Doctrine resolve the drop order we avoid
+            // DB server constraint violation errors (e.g. in MySQL).
+            $fromSchema = $sm->createSchema();
+            $toSchema = clone $fromSchema;
+
+            foreach ($newSchema->getTables() as $newTable) {
+                if ($toSchema->hasTable($newTable->getName())) {
+                    $toSchema->dropTable($newTable->getName());
+                }
+            }
+
+            $sqls = $fromSchema->getMigrateToSql($toSchema, $conn->getDatabasePlatform());
+            foreach ($sqls as $sql) {
+                $conn->executeQuery($sql);
+            }
+        }
+    }
+
+    /**
+     * Drops all the database tables that
+     * a) are prefixed with the camelcased package handle of this package
+     * b) are not linked to any existing entity within this package
+     */
+    public function dropObsoleteDatabaseTables()
+    {
+        $em = $this->getEntityManager();
+        $conn = $em->getConnection();
+        $sm = $conn->getSchemaManager();
+        $cmf = $em->getMetadataFactory();
+
+        $existing = array();
+
+        $classes = $this->getPackageEntities();
+        foreach ($classes as $cls) {
+            $md = $cmf->getMetadataFor($cls);
+            $existing[] = $md->getTableName();
+        }
+
+        $fromSchema = $sm->createSchema();
+        $toSchema = clone $fromSchema;
+
+        $prefix = camelcase($this->getPackageHandle());
+        foreach ($fromSchema->getTables() as $tbl) {
+            if (strpos($tbl->getName(), $prefix) === 0 && !in_array($tbl->getName(), $existing)) {
+                $toSchema->dropTable($tbl->getName());
+            }
+        }
+
+        $sqls = $fromSchema->getMigrateToSql($toSchema, $conn->getDatabasePlatform());
+        foreach ($sqls as $sql) {
+            $conn->executeQuery($sql);
+        }
+    }
+
+    public function getPackageEntities()
+    {
+        return $this->getPackageEntitiesInDir($this->getPackagePath() . '/src/Entity');
+    }
+
+    public function getPackageProxiesPath()
+    {
+        return $this->getPackagePath() . '/' . $this->DIRNAME_PROXIES;
+    }
+
+    protected function modifyEntityManagerConfiguration(\Doctrine\ORM\Configuration $config)
+    {
+        $config->setProxyDir($this->getPackageProxiesPath());
+    }
+
+    protected function getPackageEntitiesInDir($dir)
+    {
+        $classes = array();
+        
+        if (is_dir($dir)) {
+            $end = '/src/Entity';
+            $subdir = substr($dir, strrpos($dir, $end)+strlen($end));
+            
+            $cls = get_class($this);
+            $ns = substr($cls, 0, strrpos($cls, '\\')) . '\Src\Entity' . str_replace('/', '\\', $subdir);
+            
+            if ($handle = opendir($dir)) {
+                while (false !== ($entry = readdir($handle))) {
+                    if ($entry != '.' && $entry != '..') {
+                        $target = $dir . '/' . $entry;
+                        if (is_dir($target)) {
+                            $classes = array_merge($classes, $this->getPackageEntitiesInDir($target));
+                        } else {
+                            $classes[] = $ns . '\\' . substr($entry, 0, strrpos($entry, '.'));
+                        }
+                    }
+                }
+                closedir($handle);
+            }
+        }
+
+        return $classes;
+    }
+
+}
